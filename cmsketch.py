3a1532ac48222a67ce628e952622c84eae6cbb9f
Leonardo Taccari <leonardo.taccari@gmail.com>
2019-10-26 00:52:51 +0200

    Add simple example

--- cmsketch.py
+++ cmsketch.py
@@ -3,16 +3,16 @@ import numpy as np
 class CountMinSketch:
 
     def __init__(self, d: int, w: int):
-        """ 
+        """
         Keeps an approximate count of each distinct values.
-        
+    
         [1] G. Cormode, S. Muthukrishnan. An Improved Data Stream Summary:
                                           The Count-Min Sketch and its Applications
         [2] G. Cormode, S. Muthukrishnan. Approximating Data with the Count-Min Data Structure
         """
         self.d, self.w = d, w
         self.X = np.zeros((d, w), dtype=int)
-        self.p = 2**31 - 1 
+        self.p = 2**31 - 1
         self.a = np.random.randint(self.p, size=d)
         self.b = np.random.randint(self.p, size=d)
 
@@ -27,4 +27,27 @@ class CountMinSketch:
 
     def count(self, x: int):
         return min(self.X[range(self.d), self.hash(x)])
+
+    def __getitem__(self, x: int):
+        return self.count(x)
+
+
+
+if __name__ == '__main__':
+    from matplotlib import pyplot as plt
+    from collections import Counter
+    a = np.random.zipf(1.3, 100_000)
+    counts = Counter(a)
+
+    cm = CountMinSketch(3, 100)
+    for x in a:
+        cm.update(x)
+
+    values = sorted(counts)
+    real_counts = [counts[v] for v in values]
+    estimates = [cm[v] for v in values]
+
+    plt.plot(values[:100], real_counts[:100])
+    plt.plot(values[:100], estimates[:100])
+    plt.show()
 
